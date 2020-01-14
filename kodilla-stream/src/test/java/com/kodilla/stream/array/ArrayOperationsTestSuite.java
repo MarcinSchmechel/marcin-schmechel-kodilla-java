a6467ef91ac15dcdf4aaac48f9ec04db1bac14f2
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-01-14 21:24:14 +0100

    Zadanie 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,21 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.kodilla.stream.array.ArrayOperations.getAverage;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage(){
+        //Given
+        int[] teatArray = {1,2,3,4,5,6};
+
+        //When
+        double calculatedAverage = getAverage(teatArray);
+        double expectedAverage = 3.5;
+
+        //Then
+        Assert.assertEquals(expectedAverage,calculatedAverage,0.01);
+    }
+}
