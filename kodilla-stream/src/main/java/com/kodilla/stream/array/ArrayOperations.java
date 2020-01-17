e77789f914fb8dda2eb876b8816ad2d6b2ecf1b2
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-01-18 00:07:13 +0100

    Zadanie 7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,18 +4,12 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    public static double getAverage(int[] numbers) {
-        double result = 0;
-        if (numbers.length!=0) {
-            IntStream.range(0, numbers.length)
-                    .forEach(x -> System.out.println(numbers[x]));
-            OptionalDouble average = IntStream.range(0, numbers.length)
-                    .map(n -> numbers[n])
-                    .average();
-            result = average.getAsDouble();
-        } else {
-            System.out.println("Tablica pusta");
-        }
-        return result;
+    static double getAverage(int[] numbers) {
+        IntStream.range(0, numbers.length)
+                .forEach(x -> System.out.println(numbers[x]));
+        OptionalDouble average = IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .average();
+        return average.orElse(0);
     }
 }
