26f7d345185a2132b1225a990e98be282139677c
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-02-06 16:48:49 +0100

    Zadanie 8.4 Obsluga bledow

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -0,0 +1,46 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightFinder {
+    Map <String, Boolean> flightMap  = new HashMap<String, Boolean>(){
+        {
+            put("Warszawa", true);
+            put("Londyn", true);
+            put("Sydney", true);
+            put("Las Vegas", false);
+            put("Szczecin", false);
+            put("Koszalin", false);
+        }
+    };
+
+    public void findFlight(Flight flight) throws RouteNotFoundException {
+        if(flightMap.containsKey(flight.getArrivalAirport()) && flightMap.containsKey(flight.getDepartureAirport())){
+            if(flightMap.get(flight.getArrivalAirport()) && flightMap.get(flight.getDepartureAirport())){
+                System.out.println("Connection: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " available.");
+            } else {
+                System.out.println("Connection: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " not available.");
+            }
+        } else {
+            System.out.println("Connection: " + flight.getDepartureAirport() + " to " + flight.getArrivalAirport() + " do not exist!");
+            throw new RouteNotFoundException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FlightFinder that = (FlightFinder) o;
+
+        return flightMap.equals(that.flightMap);
+    }
+
+    @Override
+    public int hashCode() {
+        return flightMap.hashCode();
+    }
+
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/RouteNotFoundExceptionRunner.java
@@ -0,0 +1,14 @@
+package com.kodilla.exception.test;
+
+public class RouteNotFoundExceptionRunner {
+    public static void main(String[] args) {
+        FlightFinder flightFinder = new FlightFinder();
+        try {
+            flightFinder.findFlight(new Flight("Warszawa","Londyn"));
+            flightFinder.findFlight(new Flight("Warszawa","Koszalin"));
+            flightFinder.findFlight(new Flight("Warszawa","Moskwa"));
+        } catch (RouteNotFoundException e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+}
