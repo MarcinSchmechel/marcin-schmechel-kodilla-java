1a8ff69f040c12b4ad3c6f5b4c9adf19dfbea8b1
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-03-15 18:27:20 +0100

    Zadanie 13.1 - poprawiono wstawianie separatora

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPatternsMain.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/GoodPatternsMain.java
@@ -2,19 +2,22 @@ package com.kodilla.good.patterns;
 
 import com.kodilla.good.patterns.challenges.MovieStore;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class GoodPatternsMain {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies();
+
         Map<String, List<String>> flatMapMovies = new HashMap<>(movieStore.getMovies());
-        flatMapMovies
+        String moviesJoined = flatMapMovies
                 .values()
                 .stream()
-                .flatMap(x -> x.stream())
-                .forEach(result -> System.out.print(result + " ! "));
+                .flatMap(Collection::stream)
+                .collect(Collectors.joining(" ! "));
+        System.out.println(moviesJoined);
     }
 }
