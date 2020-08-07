ee60496de55d0ba8f431d4a473b5278791ef59ec
MarcinSchmechel <marcin.schmechel@o2.pl>
2020-08-07 19:54:14 +0200

    zadanie 24.5 - Poprawki - fully qualified path

--- kodilla-paterns2/src/main/java/com/kodilla/paterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-paterns2/src/main/java/com/kodilla/paterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,6 +1,5 @@
 package com.kodilla.paterns2.adapter.bookclasifier;
 
-import com.kodilla.paterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.paterns2.adapter.bookclasifier.librarya.Classifier;
 import com.kodilla.paterns2.adapter.bookclasifier.libraryb.BookSignature;
 
@@ -10,10 +9,10 @@ import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.paterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.paterns2.adapter.bookclasifier.libraryb.Book> books = new HashMap<>();
 
-        for(Book theBook : bookSet){
+        for(com.kodilla.paterns2.adapter.bookclasifier.librarya.Book theBook : bookSet){
             books.put(new BookSignature(theBook.getSignature()),
                     new com.kodilla.paterns2.adapter.bookclasifier.libraryb.Book(theBook.getAuthor(),
                             theBook.getTitle(),
